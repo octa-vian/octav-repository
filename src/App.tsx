9f859b9868864a4557d1a4d89e28901c58023251
octavian <octab39@gmail.com>
2025-03-18 22:26:45 +0700

    gh

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -10,56 +11,169 @@ import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import StarBorder from "@mui/icons-material/StarBorder";
 import VerifiedUser from "@mui/icons-material/VerifiedUser";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 function App() {
+  const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
   const [openItems, setOpenItems] = useState<Record<number, boolean>>({});
   const [searchTerm, setSearchTerm] = useState(""); // State untuk pencarian
-  const [data, setData] = useState<Mahasiswa[]>([]);
+  const [data, setData] = useState<Repository[]>([]);
   const [notFound, setNotFound] = useState(false);
-  const [githubUser, setGithubUser] = useState(null);
-  const [githubQuery, setGithubQuery] = useState("");
+  const [query, setQuery] = useState("");
+  interface GitHubUser {
+    id: number;
+    login: string;
+    name?: string; // Optional, karena tidak selalu ada dalam API response
+    avatar_url: string;
+  }
+  const [users, setUsers] = useState<GitHubUser[]>([]);
+  const [repos, setRepos] = useState<Repository[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<number | null>(null);
 
   const handleClick = (id: number) => {
     setOpenItems((prev) => ({
       ...prev,
       [id]: !prev[id], // Toggle hanya untuk item yang diklik
     }));
+    setSelectedUser(id);
   };
 
-  interface Point {
+  interface Repository {
     id: number;
-    title: string;
-    ket: string;
-    nilai_point: string;
-  }
-
-  interface Mahasiswa {
-    id: number;
+    node_id: string;
     name: string;
-    point: Point[];
+    full_name: string;
+    private: boolean;
+    owner: Owner;
+    html_url: string;
+    description: string | null;
+    fork: boolean;
+    url: string;
+    created_at: string;
+    updated_at: string;
+    pushed_at: string;
+    git_url: string;
+    ssh_url: string;
+    clone_url: string;
+    svn_url: string;
+    homepage: string | null;
+    size: number;
+    stargazers_count: number;
+    watchers_count: number;
+    language: string | null;
+    has_issues: boolean;
+    has_projects: boolean;
+    has_downloads: boolean;
+    has_wiki: boolean;
+    has_pages: boolean;
+    has_discussions: boolean;
+    forks_count: number;
+    mirror_url: string | null;
+    archived: boolean;
+    disabled: boolean;
+    open_issues_count: number;
+    license: string | null;
+    allow_forking: boolean;
+    is_template: boolean;
+    web_commit_signoff_required: boolean;
+    topics: string[];
+    visibility: string;
+    forks: number;
+    open_issues: number;
+    watchers: number;
+    default_branch: string;
   }
 
-  React.useEffect(() => {
-    handleSearch();
-  }, [searchTerm]);
+  interface Owner {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+  }
 
-  const handleSearch = async () => {
+  // React.useEffect(() => {
+  //   //handleSearch();
+  //   handleSearch2();
+  // }, [searchTerm]);
+
+  const searchUsers = async () => {
+    if (!searchTerm) return;
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.github.com/search/users?q=${searchTerm}&per_page=5`,
+        {
+          headers: {
+            Authorization: `token ${GITHUB_TOKEN}`,
+          },
+        }
+      );
+      setUsers(response.data.items);
+      console.log("Success", response.data.items);
+    } catch (error) {
+      console.error("Error fetching users", error);
+    }
+    setLoading(false);
+  };
+
+  const fetchRepos = async (username: string) => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${username}/repos`,
+        {
+          headers: {
+            Authorization: `token ${GITHUB_TOKEN}`,
+          },
+        }
+      );
+      setRepos([]);
+      setRepos(response.data);
+    } catch (error) {
+      console.error("Error fetching repositories", error);
+    }
+    setLoading(false);
+  };
+
+  const handleSearch2 = async () => {
     if (!searchTerm) return;
 
     try {
       const response = await fetch(
-        `http://localhost:5001/mahasiswa/search?name=${searchTerm}`
+        `https://api.github.com/users/${searchTerm}/repos`,
+        {
+          headers: {
+            Authorization: `token ${GITHUB_TOKEN}`,
+          },
+        }
       );
-      const result: Mahasiswa[] = await response.json();
+
+      const result: Repository[] = await response.json();
+      const resultOwner: Owner[] = await response.json();
 
       if (response.ok) {
         setData(result);
-        setNotFound(false);
-        console.log("Error fetching data:", result);
+        setNotFound(result.length === 0);
+        console.log("log result 2:", result);
       } else {
         setData([]);
         setNotFound(true);
-        console.log("Error fetching data:", result);
+        console.error("Error response:", result);
       }
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -68,41 +182,6 @@ function App() {
     }
   };
 
-  const dataList = [
-    {
-      id: 1,
-      name: "Anis SukmaWati",
-      point: [
-        { id: 1, title: "Point 1", ket: "point Normal", nilai_point: "12" },
-      ],
-    },
-    {
-      id: 2,
-      name: "Zero Vian",
-      point: [
-        { id: 1, title: "Point 1", ket: "point Normal", nilai_point: "50" },
-      ],
-    },
-    {
-      id: 3,
-      name: "Bagaskara Octavian",
-      point: [
-        { id: 1, title: "Point 1", ket: "point Normal", nilai_point: "5" },
-        { id: 2, title: "Point 2", ket: "point Normal", nilai_point: "10" },
-      ],
-    },
-    {
-      id: 4,
-      name: "Bagaskara Aditya",
-      point: [
-        { id: 1, title: "Point 1", ket: "point Normal", nilai_point: "5" },
-        { id: 2, title: "Point 2", ket: "point Normal", nilai_point: "100" },
-        { id: 3, title: "Point 2", ket: "point Normal", nilai_point: "100" },
-      ],
-    },
-  ];
-
-  // **Filter dataList berdasarkan nama**
   const filteredData = data.filter((item) =>
     item.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -111,17 +190,19 @@ function App() {
     <div className="App">
       <div className="content">
         <div id="todo-header" className="header">
-          <h1>Student Data</h1>
+          <h1>GitHub Explorer</h1>
           <input
             type="text"
-            placeholder="Search name..."
+            placeholder="Search User..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          {/* <span className="add-button">Search</span> */}
+          <span onClick={searchUsers} className="add-button">
+            Search
+          </span>
         </div>
 
-        {searchTerm.length > 0 ? (
+        {users.length > 0 ? (
           <p
             style={{
               paddingInline: "16px",
@@ -139,45 +220,78 @@ function App() {
         >
           {searchTerm === "" ? (
             <p style={{ padding: "10px", textAlign: "center", color: "#888" }}>
-              Please search for student data...
+              Please search for repository data...
             </p>
-          ) : filteredData.length > 0 ? (
-            filteredData.map((item: Mahasiswa) => (
+          ) : users.length > 0 ? (
+            users.map((item) => (
               <React.Fragment key={item.id}>
-                <ListItemButton onClick={() => handleClick(item.id)}>
-                  <ListItemIcon>
-                    <VerifiedUser />
-                  </ListItemIcon>
-                  <ListItemText primary={item.name} />
-                  {openItems[item.id] ? <ExpandLess /> : <ExpandMore />}
+                <ListItemButton
+                  onClick={() => {
+                    handleClick(item.id);
+                    fetchRepos(item.login);
+                  }}
+                >
+                  {/* <ListItemIcon>
+                    <VerifiedUser/>
+                  </ListItemIcon> */}
+                  <img
+                    src={item.avatar_url}
+                    style={{
+                      height: 30,
+                      width: 30,
+                      marginLeft: 6,
+                      marginRight: 12,
+                      marginBottom: 6,
+                      marginTop: 6,
+                      borderRadius: 100,
+                    }}
+                  />
+                  <ListItemText primary={item.login} />
+                  {openItems[item.id] &&
+                  repos.length > 0 &&
+                  selectedUser === item.id ? (
+                    <ExpandLess />
+                  ) : (
+                    <ExpandMore />
+                  )}
                 </ListItemButton>
-                <Collapse in={openItems[item.id]} timeout="auto" unmountOnExit>
+                <Collapse
+                  in={
+                    repos.length > 0 && selectedUser === item.id
+                      ? openItems[item.id]
+                      : false
+                  }
+                  timeout="auto"
+                  unmountOnExit
+                >
                   <List
                     style={{ background: "#d5d4d3" }}
                     component="div"
                     disablePadding
                   >
-                    {item.point.map((point) => (
-                      <ListItemButton key={point.id} sx={{ pl: 4 }}>
-                        <ListItemText
-                          primary={point.title}
-                          secondary={point.ket}
-                        />
-                        <ListItemIcon>
-                          <ListItemText primary={point.nilai_point} />
-                          <StarBorder />
-                        </ListItemIcon>
-                      </ListItemButton>
-                    ))}
+                    {repos.length > 0
+                      ? repos.map((repo) => (
+                          <ListItemButton key={repo.id} sx={{ pl: 4 }}>
+                            <ListItemText
+                              primary={repo.full_name}
+                              secondary={repo.description}
+                            />
+                            <ListItemIcon>
+                              <ListItemText primary={repo.watchers_count} />
+                              <StarBorder />
+                            </ListItemIcon>
+                          </ListItemButton>
+                        ))
+                      : null}
                   </List>
                 </Collapse>
               </React.Fragment>
             ))
-          ) : (
+          ) : users.length < 0 ? (
             <p style={{ padding: "10px", textAlign: "center", color: "red" }}>
               Data not found
             </p>
-          )}
+          ) : null}
         </List>
       </div>
     </div>
